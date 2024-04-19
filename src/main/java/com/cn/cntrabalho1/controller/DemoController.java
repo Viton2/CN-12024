097cf3ccba95bcbe3d6f8fbc310b50c2a4f793a9
vitor.oliveira2 <vitor.oliveira2@sempreceub.com>
2024-04-19 20:08:20 -0300

    commit

--- src/main/java/com/cn/cntrabalho1/controller/DemoController.java
+++ src/main/java/com/cn/cntrabalho1/controller/DemoController.java
@@ -1,11 +1,9 @@
 package com.cn.cntrabalho1.controller;
 
-import com.cn.cntrabalho1.service.DemoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/")
